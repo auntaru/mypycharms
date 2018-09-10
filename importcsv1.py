b75ffb70592ed63fe03d3375b3b8553e95be54b6
auntaru <alex_untaru@yahoo.com>
2018-09-10 11:52:58 +0300

    1st python commit

---
+++ importcsv1.py
@@ -0,0 +1,13 @@
+import csv
+
+with open('employee_birthday.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print('Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print row[0], 'works in the  ', row[1] , 'department, and was born in ', row[2]
+            line_count += 1
+    print('Processed {line_count} lines.')
